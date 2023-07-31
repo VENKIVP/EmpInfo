0a64832f816c55d349b37858b5d529b6604aace1
vekni <thinakaranindhumathi@gmail.com>
2023-07-31 16:19:58 +0530

    Bugs fix

--- EmpInfo/Controllers/EmployeeController.cs
+++ EmpInfo/Controllers/EmployeeController.cs
@@ -12,7 +12,28 @@ namespace EmpInfo.Controllers {
     {
         public IActionResult Index()
         {
-            return View();
+            string connectionString = "Server=localhost;Database=Employee;Trusted_Connection=True";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) FROM [dbo].[EmpInfoTable]");
+
+            sqlCommand.Connection = sqlConnection;
+
+            int RecordCount = Convert.ToInt32(sqlCommand.ExecuteScalar());
+            SqlCommand sqlCommand1 = new SqlCommand("SELECT COUNT(*) FROM [dbo].[EmpSalary1]");
+
+            sqlCommand1.Connection = sqlConnection;
+
+            int RecordCount1 = Convert.ToInt32(sqlCommand1.ExecuteScalar());
+
+            EmployeeViewModel viewModel = new EmployeeViewModel();
+            viewModel.Total = RecordCount;
+            viewModel.Salary.Total = RecordCount1;
+            
+            return View(viewModel);
         }
         [HttpGet]
         public List<EmployeeViewModel> DatatableSearch()
@@ -44,16 +65,77 @@ namespace EmpInfo.Controllers {
         }
         
         [HttpGet]
-        public async Task<IActionResult> AddEmployee(int selectedid)
+        public IActionResult AddEmployee(int id)
         {
-            
+            ReturnArgs r = new ReturnArgs();
             EmployeeViewModel employee = new EmployeeViewModel();
-            return PartialView("Partial/_AddEmployee", employee);
+            if (id > 0)
+            {
+                SqlConnection conn = new SqlConnection("Server=localhost;Database=Employee;Trusted_Connection=True;");
+                conn.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM dbo.EmpInfoTable where ID ="+id, conn);
+                DataTable dt = new DataTable();
+                dt.Load(command.ExecuteReader());
+                List<EmployeeViewModel> data = new List<EmployeeViewModel>();
+                foreach (DataRow row in dt.Rows)
+                {
+                    EmployeeViewModel dataItem = new EmployeeViewModel
+                    {
+                        Id = Convert.ToInt32(row["Id"]),
+                        FirstName = row["FirstName"].ToString(),
+                        LastName = row["LastName"].ToString(),
+                        City = row["City"].ToString(),
+                        Zip = row["Zip"].ToString(),
+                        CreatedDate = Convert.ToDateTime(row["CreatedDate"])
+                    };
+                    data.Add(dataItem);
+                }
+                employee = data.FirstOrDefault();
+            }
+            r.View = PartialView("Partial/AddEmployee",employee);
+            
+            return Json(r);
+        }
+
+
+        [HttpGet]
+        public IActionResult AddSalary(int id)
+        {
+            ReturnArgs r = new ReturnArgs();
+            EmployeeViewModel employee = new EmployeeViewModel();
+            if (id > 0)
+            {
+                SqlConnection conn = new SqlConnection("Server=localhost;Database=Employee;Trusted_Connection=True;");
+                conn.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM dbo.EmpSalary1 where Id =" + id, conn);
+                DataTable dt = new DataTable();
+                dt.Load(command.ExecuteReader());
+                List<EmployeeSalary> data = new List<EmployeeSalary>();
+                foreach (DataRow row in dt.Rows)
+                {
+                    EmployeeSalary dataItem = new EmployeeSalary
+                    {
+                        Id = Convert.ToInt32(row["Id"]),
+                        EmployeeId = Convert.ToInt32(row["EmployeeId"]),
+                        Amount = Convert.ToDecimal(row["Amount"]),
+                        SalaryDate = Convert.ToDateTime(row["SalaryDate"]),
+                        CreatedDate = Convert.ToDateTime(row["CreatedDate"])
+                    };
+                    data.Add(dataItem);
+                }
+                employee.Salary = data.FirstOrDefault();
+            }
+            r.View = PartialView("Partial/AddEmployeeSalary", employee);
+
+            return Json(r);
         }
         [HttpPost]
         public int saveEmployee(EmployeeViewModel employeeViewModel)
         {
             int id = 0;
+            employeeViewModel.Id = employeeViewModel.Id + 1;
             try
             {
                 using (SqlConnection connection = new("Server=localhost;Database=Employee;Trusted_Connection=True;"))
@@ -65,7 +147,7 @@ namespace EmpInfo.Controllers {
                         CommandTimeout = 600,
                         CommandType = CommandType.StoredProcedure
                     };
-                    command.Parameters.AddWithValue("id", employeeViewModel.Id);
+                    command.Parameters.AddWithValue("@id", employeeViewModel.Id);
                     command.Parameters.AddWithValue("@first_name", employeeViewModel.FirstName);
                     command.Parameters.AddWithValue("@last_name", employeeViewModel.LastName);
                     command.Parameters.AddWithValue("@City", employeeViewModel.City);
@@ -73,6 +155,43 @@ namespace EmpInfo.Controllers {
                     command.Parameters.AddWithValue("@CreateDate", employeeViewModel.CreatedDate);
                     id = command.ExecuteNonQuery();
                     connection.Close();
+                    if (id == 1)
+                        id = employeeViewModel.Id + 1;
+                }
+
+                return id;
+            }
+            catch (Exception ex)
+            {
+                return id;
+            }
+        }
+
+        [HttpPost]
+        public int saveSalary(EmployeeViewModel employeeViewModel)
+        {
+            int id = 0;
+            employeeViewModel.Salary.Id = employeeViewModel.Salary.Id + 1;
+            try
+            {
+                using (SqlConnection connection = new("Server=localhost;Database=Employee;Trusted_Connection=True;"))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new("dbo.EmployeeSalaryList", connection)
+                    {
+                        CommandTimeout = 600,
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    command.Parameters.AddWithValue("@Id", employeeViewModel.Salary.Id);
+                    command.Parameters.AddWithValue("@EmployeeId", employeeViewModel.Salary.EmployeeId);
+                    command.Parameters.AddWithValue("@Amount", employeeViewModel.Salary.Amount);
+                    command.Parameters.AddWithValue("@SalaryDate", employeeViewModel.Salary.SalaryDate);
+                    command.Parameters.AddWithValue("@CreateDate", employeeViewModel.Salary.CreatedDate);
+                    id = command.ExecuteNonQuery();
+                    connection.Close();
+                    if (id == 1)
+                        id = employeeViewModel.Salary.Id + 1;
                 }
 
                 return id;
--- EmpInfo/Models/EmployeeViewModel.cs
+++ EmpInfo/Models/EmployeeViewModel.cs
@@ -1,12 +1,37 @@
-﻿namespace EmpInfo.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Schema;
+
+namespace EmpInfo.Models
 {
     public class EmployeeViewModel
     {
+        public EmployeeViewModel() {
+            Salary = new EmployeeSalary();
+        }
+        public EmployeeSalary Salary { get; set; }
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string City { get; set; }
         public string Zip { get; set; }
         public DateTime CreatedDate { get; set; }
+        public int Total { get; set; }
+    }
+    public class EmployeeSalary
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime SalaryDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Total { get; set; }
+    }
+    public class ReturnArgs
+    {
+        public ReturnArgs()
+        {
+        }
+
+        public PartialViewResult View { get; set; }
     }
 }
